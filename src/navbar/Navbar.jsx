b4b8516c02c5ac1c829e582e214a0934311ccff0
selim yay <selimxyay@gmail.com>
2023-12-31 01:12:51 +0300

    made a responsive navbar

---
+++ src/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import "./navbar.css";
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            <h3 className="navbar__text">Home</h3>
+            <h3 className="navbar__text">Log In</h3>
+        </div>
+    );
+};
+
+export default Navbar;
